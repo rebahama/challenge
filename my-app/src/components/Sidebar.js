6287579b0f06bd9443ec9e93e3dd3839d4561481
rebahama <wiri@hotmail.se>
2022-08-04 23:02:48 +0000

    challagne one

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import css from "./css/Sidebar.module.css"
+
+const SideBar = ()=> <div className = {css.sidebar} >
+    <a href="#">My photos </a>
+    <a href="#"> My ilustrations </a>
+    <a href="#"> My paintings </a>
+</div>
+
+export default SideBar

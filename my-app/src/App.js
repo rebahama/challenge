0b8c02e730c4af6ee838519605eca8c6ed5fd497
rebahama <wiri@hotmail.se>
2022-08-08 02:24:11 +0000

    challenge two text and state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import css from './App.module.css';
 import SideBar from './components/Sidebar';
+import NavBarSimple from './components/navBarSimple';
 
 
 function App() {
   return (
     <div className={css.App}>
       {<SideBar/>}
+      {<NavBarSimple/>}
     </div>
   );
 }
---
+++ my-app/src/components/navBarSimple.js
@@ -0,0 +1,47 @@
+import React from "react";
+import css from "./css/Sidebar.module.css"
+
+
+class NavBarSimple extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            message: "Hello, guest!",
+            buttonText: "Log in"
+        }
+    }
+////
+
+
+handleclick = () => {
+    this.setState((prevState) => ({
+        message: prevState.message === "Hello, guest!" ? "Welcome back, user!" : "Hello, guest!",
+        buttonText: prevState.buttonText === "Log out" ? "Log in" : "Log out",
+    }), ()=> console.log(this.state.message))
+    
+}
+
+    render(){
+        return (<div className = {css.NavBar}>
+            <h1> My gallery </h1>
+            
+            <div>
+            
+            <span> {this.state.message}</span>
+            
+            <button onClick={() => this.handleclick()}> {this.state.buttonText} </button>
+          
+          </div>
+                </div>)
+        }
+
+
+};
+
+
+
+
+
+
+export default NavBarSimple
